36fc52ffd29d4a965eceb440beb75cdb6ee44455
Edilan Barbosa <edilanbarbosa.contato@gmail.com>
2024-08-27 01:31:14 -0300

    Declarações múltiplas de variáveis, constantes #define - @cfbcursos C++

---
+++ aulas/aula-005/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define canal cout << "Canal fessor Bruno \n\n"
+
+int main () {
+	
+	int vidas = 3, tiros = 500, life = 100;
+	
+	cout << pi << endl;
+	canal;
+	
+	return 0;
+}
