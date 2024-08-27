00c7ed3207d7489df0cdca8a77bce1f292d097ac
Edilan Barbosa <edilanbarbosa.contato@gmail.com>
2024-08-27 01:33:25 -0300

    Invertendo valores de variáveis - @cfbcursos C++

---
+++ aulas/aula-008/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	int num = 10;
+	
+	cout << num << endl;
+	
+//	num = num * -1;
+	num =- num;
+	
+	cout << num << endl;
+	
+	return 0;
+}
